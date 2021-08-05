d2d744502b892ea7a7762f753a03ce8808780548
Artur <ites777@gmail.com>
2021-08-05 18:19:47 +0300

    add Server

---
+++ server/db/models/routes.js
@@ -0,0 +1,49 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Routes extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+     static associate({Reference, Photos, Reviews}) {
+      this.hasMany(Reference, {foreignKey: "routes_id"})
+      this.hasMany(Photos, {foreignKey: "routes_id"})
+      this.hasMany(Reviews, {foreignKey: "routes_id"})
+    }
+  };
+  Routes.init({
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    difficalty: {
+      type: DataTypes.STRING,
+    },
+    rating: {
+      type: DataTypes.STRING,
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    length: {
+      type: DataTypes.INTEGER,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    coordinates: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
+  }, {
+    sequelize,
+    modelName: 'Routes',
+  });
+  return Routes;
+};
